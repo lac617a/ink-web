d82f9d8a3272a2e6c030f208fca4bc92bfcb3125
lac617a <botlacrita617@gmail.com>
2021-05-26 12:26:51 -0500

    configurando tools to config

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,28 @@
+import smtplib
+
+# from django.conf import settings
 from django.test import TestCase
+from email.mime.text import MIMEText
+from config.settings import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER,EMAIL_HOST_PASSWORD
 
 # Create your tests here.
+
+def send_email():
+  try:
+    # Establecemos conexion con el servidor smtp de gmail
+    mailServer = smtplib.SMTP(EMAIL_HOST,EMAIL_PORT)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(EMAIL_HOST_USER,EMAIL_HOST_PASSWORD)
+    # Construimos el mensaje simple
+    mensaje = MIMEText("""Este es el mensaje de las narices""")
+    mensaje['From'] = EMAIL_HOST_USER
+    mensaje['To'] = EMAIL_HOST_USER
+    mensaje['Subject'] = "Tienes un correo"
+    # Envio del mensaje
+    mailServer.sendmail(EMAIL_HOST_USER,EMAIL_HOST_USER,mensaje.as_string())
+  except Exception as e:
+    print(e)
+
+send_email()
