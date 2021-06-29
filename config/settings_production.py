af8ee5992bd86d330ece11ff76a1ab3113595307
lac617a <botlacrita617@gmail.com>
2021-06-29 13:55:14 -0500

    tools to config in setting-productions

--- config/settings_production.py
+++ config/settings_production.py
@@ -4,7 +4,7 @@ from .AWS import *
 # s3 public media settings
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'tools.storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
